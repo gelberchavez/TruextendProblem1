b97371422ee7bc10773912d66e9e6a556fd78e81
gelberchavez <geltigre@gmail.com>
2020-02-16 02:50:01 -0400

    Adicionamos Proyectos y primera funcionalidad

---
+++ Truextend.StudentManagement.Core/ConsoleApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Truextend.StudentManagement.Core.Bussiness;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Start...");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Import of records from CSV File ...");
+            //Call to method
+            ImportStudentsCSV();
+            Console.WriteLine("Press any key to exit!!!!!");
+            Console.ReadKey();
+        }
+        #region Methods
+        // Method to import records from the  CSV file
+        private static void ImportStudentsCSV()
+        {   //Recover CSV file path 
+            string sruta = System.Configuration.ConfigurationManager.AppSettings["RutaFile"];
+
+            string[] lines = File.ReadAllLines(sruta);
+            string PreMessage = "ConsoleApp.exe input.csv ";
+            foreach (var line in lines)
+            {
+                //Separate comma values, for each line
+                var values = line.Split(',');
+                //Instance the object
+                clsStudent oStudent = new clsStudent();
+                bool sw;
+                // Use the object method to Insert 
+                sw = oStudent.AddStudent(values[0].ToString(), values[1].ToString(), values[2].ToString(), FormatDate(values[3].ToString()));
+
+                if (sw)
+                {
+                    Console.WriteLine(PreMessage + "name=" + values[1].ToString());// name
+                    Console.WriteLine(PreMessage + "type=" + values[0].ToString());// type
+                    string sGender= (values[2].ToString()=="F") ? "female" : "male";
+                    Console.WriteLine(PreMessage + "gender=" + sGender);// gender
+                    Console.WriteLine("Registered line... ");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+        }
+        //Method for format Datetimestamp
+        public static DateTime FormatDate(string pStringDateFormatCSV)
+        {
+            DateTime ReturnDate;
+            int formatYYYY = Convert.ToInt32(pStringDateFormatCSV.Substring(0, 4));//Year
+            int formatMM = Convert.ToInt32(pStringDateFormatCSV.Substring(4, 2));//Month
+            int formatDD = Convert.ToInt32(pStringDateFormatCSV.Substring(6, 2));//Day
+            int formatHH = Convert.ToInt32(pStringDateFormatCSV.Substring(8, 2));//Hour
+            int formatmm = Convert.ToInt32(pStringDateFormatCSV.Substring(10, 2));//Minute
+            int formatSS = Convert.ToInt32(pStringDateFormatCSV.Substring(12, 2));//Second
+            ReturnDate = new DateTime(formatYYYY, formatMM, formatDD, formatHH, formatmm, formatSS);
+            return ReturnDate;// Returns the required date format
+        }
+
+        #endregion
+    }
+
+}
---
+++ Truextend.StudentManagement.Core/Truextend.StudentManagement.Core.Bussiness/clsStudent.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Truextend.StudentManagement.Core.Entities;
+
+namespace Truextend.StudentManagement.Core.Bussiness
+{
+    public class clsStudent : clsStudentBE
+    {
+        #region Constructor
+        //Constructor de la clase
+        public clsStudent()
+        {
+            this.Id = 0;
+            this.StudentType = string.Empty;
+            this.StudentName = string.Empty;
+            this.Gender = string.Empty;
+            this.DateTimeStamp = DateTime.Now;
+        }
+
+        #endregion
+        //String Conexion
+        string ConexionString = ConfigurationManager.ConnectionStrings["StudentsConexion"].ConnectionString;
+
+        #region Metodos
+        //Method to register a STUDENT in the DB.
+        public bool AddStudent(string pStudentType, string pStudentName, string pGender, DateTime pDateTimeStamp)
+        {
+            bool sw = false;
+            //Open the conexion with DB.
+            SqlConnection conexion = new SqlConnection(ConexionString);
+            conexion.Open();
+            // Instance the  command which calls the stored procedure of the DB.
+            SqlCommand command = new SqlCommand("psAddStudent", conexion);
+            command.CommandType = CommandType.StoredProcedure;
+            // Insert the parameters of  the stored procedure.
+            command.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@StudentType",
+                SqlDbType = SqlDbType.NVarChar,
+                Value = pStudentType,
+                Direction = ParameterDirection.Input
+            });
+            command.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@StudentName",
+                SqlDbType = SqlDbType.NVarChar,
+                Value = pStudentName,
+                Direction = ParameterDirection.Input
+            });
+            command.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@Gender",
+                SqlDbType = SqlDbType.NVarChar,
+                Value = pGender,
+                Direction = ParameterDirection.Input
+            });
+            command.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@DateTimeStamp",
+                SqlDbType = SqlDbType.DateTime,
+                Value = pDateTimeStamp,
+                Direction = ParameterDirection.Input
+            });
+
+            try
+            {
+                // Execute query
+                command.ExecuteNonQuery();
+                sw = true;// execute affirmative
+            }
+
+            catch(Exception e)
+            {
+                sw = false; // error
+            }
+
+            // Close the conexion.
+            conexion.Close();
+            // return boolean
+            return sw;
+        }
+
+        //Method to delete a STUDENT in the DB.
+
+
+        //Method: search by name, sorted alphabetically
+
+
+        //Method: search by type, sorted  by date,most recent to least recent.
+
+
+        //Method: search by gender and type, sorted  by date,most recent to least recent.
+
+
+        #endregion
+
+    }
+}
